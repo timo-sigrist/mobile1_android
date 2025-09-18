158d290664508f49984a70732e695f3169a9c0af
Timo <timo.sigrist@gmail.com>
2025-09-18 21:11:10 +0200

    fix

--- app/src/main/java/com/example/exampleapp/AppNavigation.kt
+++ app/src/main/java/com/example/exampleapp/AppNavigation.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
+// fix
 @Composable
 fun AppNavigation(navController: NavHostController, modifier: Modifier = Modifier) {
 
